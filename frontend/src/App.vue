b829b27d587b102baaf9fd0c57a1be30b01b69c2
اسدالله تیموری <15721@refahbank.net>
2022-03-31 11:32:54 +0430

    initializing bulma axios store, ...

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,45 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item navbar-start"><strong>WIS</strong></router-link>
+      </div>
+      <div class="navbar-menu">
+        <div class="navbar-end">
+          <template v-if="$store.state.isAuthenticated">
+            <router-link to="/dashboard" class="navbar-item">کارتابل</router-link>
+          </template>
+        </div>
+      </div>
+    </nav>
+  
+  <section class="section">
   <router-view/>
+  </section>
+  <footer class="footer"><p class="has-text-centered">Copyright (c) 2022</p></footer>
+</div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import axios from 'axios'
+export default {
+  name: 'App', 
+  beforeCreate() {
+    this.$store.commit('initializeStore')
+    const token = this.$store.state.token
+    if (token) {
+      axios.defaults.headers.common['Authorization'] = "Token "+token
+    } else {
+      axios.defaults.headers.common['Authorization'] = ""
     }
-  }
+  },
 }
+</script>
+<style lang="scss">
+@import '../node_modules/bulma';
+
+.navbar-end {
+  float: right !important;
+}
+
 </style>
